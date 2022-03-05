2856b338fb06a12f25baf6c8ec9d8821cdfc2fd9
Keigo Imai <keigo.imai@gmail.com>
2022-03-06 08:02:31 +0900

    change

--- lib/rows.ml
+++ lib/rows.ml
@@ -20,5 +20,7 @@ let cast_if_constrs_are_same :
     ('var, 'a) constr -> ('var, 'b) constr -> 'b -> 'a option =
  fun var1 var2 b -> var1.match_var (var2.make_var b)
 
-let cast_obj : ('obj, 'mt) method_ -> ('obj, 'mt2) method_ -> 'mt2 -> 'mt1 =
+let cast_obj :
+      'obj 'mt1 'mt2.
+      ('obj, 'mt1) method_ -> ('obj, 'mt2) method_ -> 'mt2 -> 'mt1 =
  fun obj1 obj2 b -> obj1.call_obj @@ obj2.make_obj b
